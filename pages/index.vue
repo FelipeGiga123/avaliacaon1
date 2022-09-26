3061bfa87ebaf2fe8358371193261bd1fb505222
FELIPE DE <110988639+FelipeGiga123@users.noreply.github.com>
2022-09-26 12:08:03 +0000

    teste

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,109 @@
-<template>
-  <section class="section">
-    <h1 class="title">Página inicial</h1>
+<template> 
+<section>
+<div>
+    
+  <section>
+    <h1 class="title mt-5">Bem-vindo ao futuro das finanças</h1>
+    <h1 class ="subtitle">São mais de 50.000 horas de conteúdo feito para você!</h1>
+    
+
+      <section class = "section">
+
+        <div>
+
+    <section>
+      <b-tabs>
+        <b-tab-item label="Pictures"> 
+            <h1 class = "subtitle"<strong>Amplie seus conhecimentos em qualquer área das finanças</strong></h1>
+      <p class = "text">Não importa se você trabalha ou apenas sonha em atuar na área de financeira, com a Agiotis você pode aprender qualquer coisa, desde a bolsa brasileria até mini-dolár asiático. </p>
+
+          <div class="columns mt-4">
+      <div class="column is-3">
+        <article class="notification is-white has-text-centered">
+          <b-image class="image" src=""></b-image>
+          <p class="title mt-5">Elétricos</p>
+          <p class="subtitle">Conheça o I3</p>
+           <b-button type="is-primary">Saiba mais</b-button>
+        </article>
+      </div>
+      <div class="column is-3 has-text-centered">
+        <article class="notification is-white">
+          <b-image class="image" src=""></b-image>
+          <p class="title mt-5">M Academy</p>
+          <p class="subtitle">M3 à todo vigor</p>
+          <b-button type="is-primary">Saiba mais</b-button>
+        </article>
+      </div>
+      <div class="column is-3">
+        <article class="notification is-white has-text-centered">
+          <b-image class="image" src=""></b-image>
+          <p class="title mt-5">Luxo 7 estrelas</p>
+          <p class="subtitle">O balanço perfeito</p>
+           <b-button type="is-primary">Saiba mais</b-button>
+        </article>
+      </div>      
+      <div class="column is-3">
+        <article class="notification is-white has-text-centered">
+          <b-image class="image" src=""></b-image>
+          <p class="title mt-5">História</p>
+          <p class="subtitle is-gray">BMW Museum</p>
+           <b-button type="is-primary">Saiba mais</b-button>
+        </article>
+      </div> 
+    </div>
+
+          
+        </b-tab-item>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        <b-tab-item label="Music">
+          Lorem <br />
+          ipsum <br />
+          dolor <br />
+          sit <br />
+          amet.
+        </b-tab-item>
+      </b-tabs>
+    </section>
+    
+    </div>
+        
+    </section>
+    
   </section>
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+    
+</div>
+</section>
 </template>
 
